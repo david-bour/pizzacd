3e697307a3a5ac2ffa2a20701649ddfa7a4a0d12
davidb <dbourdeveloper@gmail.com>
2020-05-02 09:37:21 -0700

    - fix: alembic scripts - feat: add email column; updated migration script

--- appserver/app/models/accounts.py
+++ appserver/app/models/accounts.py
@@ -4,6 +4,7 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
+    email = db.Column(db.String(64), unique=True)
 
 class AccountSchema(SQLAlchemyAutoSchema):
     class Meta:
