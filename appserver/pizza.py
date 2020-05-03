627dfd56cb555b55441f26b67956ac5abc2314a7
davidb <dbourdeveloper@gmail.com>
2020-05-02 22:17:25 -0700

    - feat: nginx prod added

--- appserver/pizza.py
+++ appserver/pizza.py
@@ -6,10 +6,14 @@ from app.models import Account
 app_config = os.environ['FLASK_APP_CONFIG']
 app = create_app(config=f'config.{app_config}')
 
+@app.route('/', methods=['GET'])
+def home():
+    return 'Home runn!!!', 200
+
 @app.cli.command('create-user')
 @click.argument('name')
 def create_user(name):
-    user = Account(username=name)
+    user = Account(username=name, email=f'{name}@pizza.co')
     db.session.add(user)
     db.session.commit()
 
